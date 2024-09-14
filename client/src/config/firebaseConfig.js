deb78f1e23767fbd929efb3a4dc8846326508527
Ashwin Sai Vagu <vagua01@pfw.edu>
2024-09-14 13:02:54 -0400

    removed firebase config

--- client/src/config/firebaseConfig.js
+++ client/src/config/firebaseConfig.js
@@ -5,13 +5,6 @@ import { getFirestore } from "firebase/firestore";
 
 // Firebase configuration object containing your project's credentials
 const firebaseConfig = {
-  apiKey: "AIzaSyAyanfVMVN2WDGtEdOIv41xGBQbx3FaTxc",
-  authDomain: "acs560-wellness-wise.firebaseapp.com",
-  projectId: "acs560-wellness-wise",
-  storageBucket: "acs560-wellness-wise.appspot.com",
-  messagingSenderId: "1071452464493",
-  appId: "1:1071452464493:web:df5c8f4d5a0f8b07666664",
-  measurementId: "G-DZ7QLKHHQL",
 };
 
 // Initialize Firebase with the provided configuration
